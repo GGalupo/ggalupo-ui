491ac2ef3c2b0162308657292bd5a1394c94a91f
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-09 20:38:49 -0300

    Stories for TextInput component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextInput, type TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text as="span" size="sm">
+          Your website
+        </Text>
+        {Story()}
+      </Box>
+    ),
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'https://yourwebsite.com',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'https://',
+  },
+}
