09796f6d035d978b222702d94372de4495bebce1
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-09 19:20:36 -0300

    Stories for Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, type AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/GGalupo.png',
+    alt: 'Gustavo Galupo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
