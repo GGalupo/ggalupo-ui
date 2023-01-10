8d291e1ef4bdb0e77fd9bff2f93322887a0a6466
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-10 08:55:27 -0300

    Stories for Checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, Checkbox, type CheckboxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box as="label" css={{ display: 'flex', gap: '$2' }}>
+        {Story()}
+        <Text as="span" size="sm">
+          Accept terms
+        </Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
---
+++ packages/react/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,16 @@
+import { type ComponentProps } from 'react'
+import { Check } from 'phosphor-react'
+
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>
+
+export const Checkbox = (props: CheckboxProps) => {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
