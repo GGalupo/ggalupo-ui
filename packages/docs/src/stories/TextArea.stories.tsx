22a4993d744caca0cc4a2d8635172faf6cadfd23
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-09 21:00:51 -0300

    Stories for TextArea component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextArea, type TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text as="span" size="sm">
+          Considerations
+        </Text>
+        {Story()}
+      </Box>
+    ),
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Type any consideration',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
