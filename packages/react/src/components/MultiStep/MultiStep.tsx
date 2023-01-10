c14e9fbe0001b919b9d54222c45c6bf3c797daba
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-10 09:56:08 -0300

    Stories for MultiStep component

---
+++ packages/react/src/components/MultiStep/MultiStep.tsx
@@ -0,0 +1,23 @@
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export type MultiStepProps = {
+  size: number
+  currentStep?: number
+}
+
+export const MultiStep = ({ size, currentStep = 1 }: MultiStepProps) => {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Step {currentStep} of {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_el, i) => {
+          const step = i + 1
+          return <Step key={i} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
