bdb48222cab8b3ea0b82c958c98e28fd026854ed
Gustavo Galupo <ggalupo2@hotmail.com>
2023-02-03 18:13:15 -0300

    Fix Button and TextArea colors

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -17,11 +17,11 @@ export const CheckboxContainer = styled(Checkbox.Root, {
   alignItems: 'center',
 
   '&[data-state="checked"]': {
-    backgroundColor: '$ignite300',
+    backgroundColor: '$grass300',
   },
 
   '&:focus, &[data-state="checked"]': {
-    border: '2px solid $ignite300',
+    border: '2px solid $grass300',
   },
 })
 
