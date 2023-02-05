e5d2b7d8a302570b4d327c27465c1b339fa67694
Gustavo Galupo <ggalupo2@hotmail.com>
2023-02-05 13:30:19 -0300

    Add size to Avatar component

--- packages/react/src/components/Avatar/Avatar.tsx
+++ packages/react/src/components/Avatar/Avatar.tsx
@@ -3,13 +3,23 @@ import { type ComponentProps } from 'react'
 
 import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
 
+type AvatarImageProps = ComponentProps<typeof AvatarImage>
+type AvatarContainerProps = ComponentProps<typeof AvatarContainer>
+
 export type AvatarProps = ComponentProps<typeof AvatarImage>
 
-export const Avatar = (props: AvatarProps) => {
+export type AvatarPropsTest = {
+  size: AvatarContainerProps['size']
+} & AvatarImageProps
+
+export const Avatar = ({
+  size = 'md',
+  ...avatarImageProps
+}: AvatarPropsTest) => {
   return (
-    <AvatarContainer>
-      <AvatarImage {...props} />
-      <AvatarFallback delayMs={600}>
+    <AvatarContainer size={size}>
+      <AvatarImage {...avatarImageProps} />
+      <AvatarFallback size={size} delayMs={600}>
         <User />
       </AvatarFallback>
     </AvatarContainer>
