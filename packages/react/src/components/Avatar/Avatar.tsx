840c1bc103c9362bb40aaac5915804094ed2ff6a
Gustavo Galupo <ggalupo2@hotmail.com>
2023-02-13 11:14:18 -0300

    Set size as optional on Avatar component props

--- packages/react/src/components/Avatar/Avatar.tsx
+++ packages/react/src/components/Avatar/Avatar.tsx
@@ -3,19 +3,17 @@ import { type ComponentProps } from 'react'
 
 import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
 
-type AvatarImageProps = ComponentProps<typeof AvatarImage>
 type AvatarContainerProps = ComponentProps<typeof AvatarContainer>
-
 export type AvatarProps = ComponentProps<typeof AvatarImage>
 
-export type AvatarPropsTest = {
-  size: AvatarContainerProps['size']
-} & AvatarImageProps
+export type AvatarComponentProps = {
+  size?: AvatarContainerProps['size']
+} & AvatarProps
 
 export const Avatar = ({
   size = 'md',
   ...avatarImageProps
-}: AvatarPropsTest) => {
+}: AvatarComponentProps) => {
   return (
     <AvatarContainer size={size}>
       <AvatarImage {...avatarImageProps} />
