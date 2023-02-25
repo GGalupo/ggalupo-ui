3c5b35178b163b819607199422bd33b9e24e4415
Gustavo Galupo <ggalupo2@hotmail.com>
2023-02-24 23:14:27 -0300

    Update Modal component props

--- packages/react/src/components/Modal/Modal.tsx
+++ packages/react/src/components/Modal/Modal.tsx
@@ -4,6 +4,6 @@ import * as Dialog from '@radix-ui/react-dialog'
 export type ModalProps = ComponentProps<typeof Dialog.Root>
 export type ModalTriggerProps = ComponentProps<typeof Dialog.Trigger>
 
-export const Modal = (props: ModalProps) => {
-  return <Dialog.Root {...props}>{props.children}</Dialog.Root>
+export const Modal = ({ children, ...props }: ModalProps) => {
+  return <Dialog.Root {...props}>{children}</Dialog.Root>
 }
